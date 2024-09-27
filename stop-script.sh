16f9510593159500cb4272f713b794cb689fb3e1
Seif <seifeldin.ibrahim@gmail.com>
2024-09-27 15:00:15 +0300

    Finished Project

--- stop-script.sh
+++
@@ -1,5 +0,0 @@
-kubectl delete deployments -n webapp --all
-kubectl delete pvc -n webapp --all
-kubectl delete pv db-pv
-kubectl delete services --all -n webapp
-kubectl delete secrets --all -n webapp
